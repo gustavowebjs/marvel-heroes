7a11c760d5bbf249436814774fcbee49fb73d3d0
gustavowebjs <gustavowebjs@gmail.com>
2021-07-24 09:50:56 -0300

    First configs and set ups

---
+++ src/theme/globalStyles.ts
@@ -0,0 +1,12 @@
+// globalStyles.js
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: #efefef;
+    font-family: 'Poppins', sans-serif;  }
+`;
+
+export default GlobalStyle;
